d9992edcdec6bcf8f0798733c1334719453cd420
juan <juancruz.23@hotmail.com>
2017-02-27 23:15:58 -0300

    db models

--- app/routes/pics.js
+++ app/routes/pics.js
@@ -2,8 +2,11 @@
 
 module.exports = function (app, appEnv) {
 
+  let PicHandler = require(appEnv.path + '/app/controllers/picHandler.server.js');
+  let picHandler = new PicHandler();
+
   app.route('/pics')
-  	.post(/*appEnv.middleware.isLoggedIn,*/ function (req, res) {
+  	.post(appEnv.middleware.isLoggedIn, function (req, res) {
       console.log("in route add pic");
       let out = {
         picUrl: req.body.picUrl,
